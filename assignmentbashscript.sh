980157baf7711df6166563b789fce170456e508e
c1007012 <c1007012@hallam.shu.ac.uk>
2022-11-30 16:07:15 +0000

    Created assignmentbashscript.sh file assignmentbashscript.sh

---
+++ assignmentbashscript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OPT1="Apache" 
+OPT2="Nginx"
+OPT3="Mariadb"
+
+echo -p "Which software would you like to install?: "
+echo "Apache"
+echo "Nginx"
+echo "Mariadb"
+read $CHOICE
+
+if [ $CHOICE=$OPT1 ]
+then
+  sudo apt install apache2
+  else
+    echo "You have decided to not install Apache"
+  fi
+   if [ $CHOICE=$OPT2 ]
+   then
+     sudo apt install nginx
+   else
+     echo "You have decided to not install Nginx"
+   fi
+   if [ $CHOICE=$OPT3 ]
+   then
+     sudo apt install mariadb-server mariadb-client
+  fi
+
+
