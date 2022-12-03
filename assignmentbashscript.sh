819eb0c65cb95e3c027e1b1425b0c1a7e81b3292
c1007012 <c1007012@hallam.shu.ac.uk>
2022-12-03 15:43:38 +0000

    Created assignmentbashscript.sh file assignmentbashscript.sh

--- assignmentbashscript.sh
+++ assignmentbashscript.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-OPT1="Apache" 
+OPT1="Apache" #variables are declared
 OPT2="Nginx"
 OPT3="Mariadb"
 
-echo -p "Which software would you like to install?: "
+echo -p "Which software would you like to install?: " #asks for user input 
 echo "Apache"
-echo "Nginx"
+echo "Nginx"   #lists the options the user has
 echo "Mariadb"
-read CHOICE
+read CHOICE  #it will read whichever option the user has chosen 
 
-if [ "$CHOICE" = "$OPT1" ]
+if [ "$CHOICE" = "$OPT1" ]       #if user types Apache then the sudo terminal command for apache will be run
 then
   sudo apt install apache2
   else
-    echo "You have decided to not install Apache"
+    echo "You have decided to not install Apache"   #if the user doesn't type Apache, then this message will appear
   fi
    if [ "$CHOICE" = "$OPT2" ]
    then
-     sudo apt install nginx
+     sudo apt install nginx    #sudo terminal command for nginx is ran if user types Nginx
    else
      echo "You have decided to not install Nginx"
    fi
     if [ "$CHOICE" = "$OPT3" ]
     then
-      sudo apt install mariadb-server mariadb-client
+      sudo apt install mariadb-server mariadb-client    #sudo terminal command for Mariadb is ran if user types Mariadb
       else
         echo "You have decided to not install Mariadb"
       fi
