94e8527bbe44ce2ca4cf7b37519463d47ab88ebd
c1007012 <c1007012@hallam.shu.ac.uk>
2022-12-07 19:15:14 +0000

    Created casestatement.sh file casestatement.sh

--- casestatement.sh
+++ casestatement.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -n "Please choose a software you would like to install (Number 1-3): "
+echo -n "Please choose a software you would like to install (Number 1-3): "   #asking for user input
 echo
 echo "1. Apache"
 echo "2. Nginx"
@@ -9,9 +9,9 @@ read CHOICE
 
 case $CHOICE in
 
-  1) sudo apt install apache2   ;;
-  2) sudo apt install nginx     ;;   
+  1) sudo apt install apache2   ;;         #terminal command to run the installation
+  2) sudo apt install nginx     ;;         #must input a number from 1-3 for the command to run
   3) sudo apt install mariadb-server mariadb-client    ;;
-  *) echo "You have not picked an option, please try again"
+  *) echo "You have not picked an option, please try again"  #if anything other than 1-3 is entered, this message will be displayed.
 esac
 
