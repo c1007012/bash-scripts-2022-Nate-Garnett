683dd7fa15aee5cb595265ac188ac5e76b78603a
c1007012 <c1007012@hallam.shu.ac.uk>
2022-12-07 11:19:00 +0000

    Created forloop.sh file forloop.sh

--- forloop.sh
+++ forloop.sh
@@ -4,3 +4,5 @@ for file in myfiles/*.txt
 do                                       #copies all .txt files from the directory 'myfiles' and places them in the directory 'copy_files'
 cp $file copy_files
 done
+
+ls copy_files                            #lists all files within the directory structure
